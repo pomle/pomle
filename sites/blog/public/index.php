c3d8445e93cb7ebda701e024fa684bc05040c0df
Pom <pontus.alexander@gmail.com>
2012-01-04 03:45:07 +0100

    - Tuned front page

--- sites/blog/public/index.php
+++ sites/blog/public/index.php
@@ -1,13 +1,31 @@
 <?
 require '../Init.inc.php';
 
-$query = "SELECT ID FROM Posts JOIN PostAlbums ON postID = ID WHERE isPublished = 1 ORDER BY timePublished DESC LIMIT 20";
+$page = (int)abs($_GET['page']);
+$pageLen = 11;
+$pageStart = $pageLen * $page;
+
+$query = \DB::prepareQuery("SELECT
+		ID
+	FROM
+		Posts
+		JOIN PostAlbums ON postID = ID
+	WHERE
+		isPublished = 1
+	ORDER BY
+		timePublished DESC
+	LIMIT %u, %u",
+	$pageStart,
+	$pageLen + 1);
+
 $postIDs = \DB::queryAndFetchArray($query);
 
 $posts = \Album::loadFromDB($postIDs);
 
 $BrickTile = new \Element\BrickTile();
 
+$i = 0;
+$hasMore = false;
 foreach($postIDs as $postID)
 {
 	if( !isset($posts[$postID]) ) continue;
@@ -29,6 +47,12 @@ foreach($postIDs as $postID)
 	$mediaPool = array_slice($mediaPool, 0, 10);
 
 	$mediaURL = \Media\Producer\BrickTile::createFromHash($mediaHash)->getTile();
+
+	if( ++$i > $pageLen )
+	{
+		$BrickTile->addItem(sprintf('/index.php?page=%u', $page+1), 'Nästa »', $mediaURL, $mediaPool);
+		break;
+	}
 
 	$BrickTile->addItem(\URL::album($Post), $Post->title, $mediaURL, $mediaPool, \Format::date($Post->timePublished));
 }
