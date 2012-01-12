b62dc93ca2a77647ae932371d8f2e46075269041
Pom <pontus.alexander@gmail.com>
2012-01-12 01:23:09 +0100

    - Overhauled Media Viewer

--- sites/blog/public/Media.php
+++ sites/blog/public/Media.php
@@ -2,44 +2,45 @@
 require '../Init.inc.php';
 
 $pageIndex = 0;
+$primaryMediaID = null;
+
 $MediaScrubber = new \Element\MediaScrubber();
 
 ### mediaHash has precedence of mediaID
-if( isset($_GET['mediaHash']) )
-	$Media_Primary = \Manager\Media::loadByHash($_GET['mediaHash']);
-elseif( isset($_GET['mediaID']) )
-	$Media_Primary = \Manager\Media::loadOneFromDB(is_array($_GET['mediaID']) ? reset($_GET['mediaID']) : $_GET['mediaID']);
-
-
-### If this image is to be displayed as a browseable group of images, get them
-if( isset($_GET['albumID']) && ($Album = \Post\Album::loadOneFromDB($_GET['albumID'])) )
+if( isset($_GET['mediaHash']) && ($Media = \Manager\Media::loadByHash($_GET['mediaHash'])) )
+{
+	$media = array($Media);
+	$primaryMediaID = $Media->mediaID;
+}
+elseif( isset($_GET['albumID']) && ($Album = \Post\Album::loadOneFromDB($_GET['albumID'])) )
+{
 	$media = $Album->media;
-elseif( isset($_GET['mediaID']) && is_array($_GET['mediaID']) )
-	$media = \Manager\Media::loadFromDB($_GET['mediaID']);
-
+	$primaryMediaID = (int)$_GET['mediaID'];
+}
+elseif( isset($_GET['mediaID']) ) ### Can be array and will be handled by Media::loadFromDB as if it is
+{
+	$media = \Manager\Media::loadFromDB((array)$_GET['mediaID']);
+	$primaryMediaID = is_array($_GET['mediaID']) ? reset($_GET['mediaID']) : $_GET['mediaID'];
+}
+else ### Fallback if none is found
+{
+	$media = array(\Manager\Media::integrateIntoLibrary(\Media\Image::createFromFile(DIR_SITE_RESOURCE . 'FileNotFound.jpg')));
+	header('HTTP/1.0 404 Not Found');
+}
 
 ### If we have an array of extra media to be browseable, prepare it
 if( isset($media) && is_array($media) )
 {
-	if( !isset($Media_Primary) ) $Media_Primary = reset($media);
-
 	$i = 0;
 	foreach($media as $Media)
 	{
-		if( $Media->mediaID == $Media_Primary->mediaID )
+		if( $Media->mediaID == $primaryMediaID )
 			$page = ($MediaScrubber->index = $i) + 1;
 
 		$MediaScrubber->addItem($Media);
 
 		$i++;
 	}
-}
-
-if( isset($Media_Primary) ) ### Fallback if none is found
-{
-	$Media_Primary = \Manager\Media::integrateIntoLibrary(\Media\Image::createFromFile(DIR_SITE_RESOURCE . 'FileNotFound.jpg'));
-	$MediaScrubber->addItem($Media_Primary);
-	header('HTTP/1.0 404 Not Found');
 }
 
 require HEADER;
